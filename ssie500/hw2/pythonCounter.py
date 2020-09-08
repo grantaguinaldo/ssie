863d0dc24fbd9393dfd6e56527fd70af8b5d33a2
Grant T. Aguinaldo <grant.aguinaldo@gmail.com>
2020-09-07 20:22:06 -0700

    added more files

---
+++ ssie500/hw2/pythonCounter.py
@@ -0,0 +1,56 @@
+def count(s):
+    count_a = s.count('a')
+    count_b = s.count('b')
+    count_c = s.count('c')
+    count_d = s.count('d')
+    count_e = s.count('e')
+    count_f = s.count('f')
+    count_g = s.count('g')
+    count_h = s.count('h')
+    count_i = s.count('i')
+    count_j = s.count('j')
+    count_k = s.count('k')
+    count_l = s.count('l')
+    count_m = s.count('m')
+    count_n = s.count('n')
+    count_o = s.count('o')
+    count_p = s.count('p')
+    count_q = s.count('q')
+    count_r = s.count('r')
+    count_s = s.count('s')
+    count_t = s.count('t')
+    count_u = s.count('u')
+    count_v = s.count('v')
+    count_w = s.count('w')
+    count_x = s.count('x')
+    count_y = s.count('y')
+    count_z = s.count('z')
+    count_space = s.count(' ')
+    
+    return {'a': count_a,
+            'b': count_b,
+            'c': count_c,
+            'd': count_d,
+            'e': count_e,
+            'f': count_f,
+            'g': count_g,
+            'h': count_h,
+            'i': count_i,
+            'j': count_j,
+            'k': count_k,
+            'l': count_l,
+            'm': count_m,
+            'n': count_n,
+            'o': count_o,
+            'p': count_p,
+            'q': count_q,
+            'r': count_r,
+            's': count_s,
+            't': count_t,
+            'u': count_u,
+            'v': count_v,
+            'w': count_w,
+            'x': count_x,
+            'y': count_y,
+            'z': count_z,
+            'space': count_space}
