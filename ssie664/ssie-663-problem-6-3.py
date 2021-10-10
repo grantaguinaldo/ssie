55759945f5d8534a003f9423763b0cbdaa43f2e7
Grant T. Aguinaldo <grant.aguinaldo@gmail.com>
2021-10-10 10:31:57 -0700

    updated problem 6-3

--- ssie664/ssie-663-problem-6-3.py
+++ ssie664/ssie-663-problem-6-3.py
@@ -4,8 +4,8 @@ def simulation(itr=None, P=None, L=None, i=None, N=None, AE=None, cdf=None):
     results_summary = []
     results_simulation = []
     
-    case_01 = (((P-L)*i) / (((1 + i)**(N)) - 1)) + ((P*i))
-    case_02 = (((P-L)*i) / (((1 + i)**(N)) - 1)) + ((P*i)+(AE))
+    cost_case_01 = (((P-L)*i) / (((1 + i)**(N)) - 1)) + ((P*i))
+    cost_case_02 = (((P-L)*i) / (((1 + i)**(N)) - 1)) + ((P*i)+(AE))
     
     for each in range(itr):
         resultsi = dict()
@@ -14,30 +14,30 @@ def simulation(itr=None, P=None, L=None, i=None, N=None, AE=None, cdf=None):
         if draw <= cdf:
             resultsi['draw'] = draw 
             resultsi['outcome'] = 'case_01' 
-            resultsi['annual_cost'] = case_01 
+            resultsi['annual_cost'] = cost_case_01 
             results_simulation.append(resultsi)
         else:
             resultsi['draw'] = draw 
             resultsi['outcome'] = 'case_02' 
-            resultsi['annual_cost'] = case_02
+            resultsi['annual_cost'] = cost_case_02
             results_simulation.append(resultsi)
             
     freq_case_01 = [each['outcome'] for each in results_simulation].count('case_01') / itr
     freq_case_02 = [each['outcome'] for each in results_simulation].count('case_02') / itr
     
     resultsk = dict()
-    resultsk['expected_value'] = (case_01 * freq_case_01) + (case_02 * freq_case_02)
+    resultsk['expected_value'] = (cost_case_01 * freq_case_01) + (cost_case_02 * freq_case_02)
     resultsk['case_01_freq'] = freq_case_01
     resultsk['case_02_freq'] = freq_case_02
-    resultsk['case_01_cost'] = case_01
-    resultsk['case_02_cost'] = case_02
+    resultsk['case_01_cost'] = cost_case_01
+    resultsk['case_02_cost'] = cost_case_02
     results_summary.append(resultsk)
     
     return results_summary, results_simulation
 
 data_A = simulation(itr=100000, P=500000, L=0, i=0.1, N=40, AE=600000, cdf=0.85)
-data_B = simulation(itr=100000, P=500000, L=0, i=0.1, N=40, AE=600000, cdf=0.95)
-data_C = simulation(itr=100000, P=500000, L=0, i=0.1, N=40, AE=600000, cdf=0.99)
+data_B = simulation(itr=100000, P=625000, L=0, i=0.1, N=40, AE=600000, cdf=0.95)
+data_C = simulation(itr=100000, P=900000, L=0, i=0.1, N=40, AE=600000, cdf=0.99)
 
 print('Expected Value of Project A: ${:,.2f}'.format(data_A[0][0]['expected_value']))
 print('Expected Value of Project B: ${:,.2f}'.format(data_B[0][0]['expected_value']))
